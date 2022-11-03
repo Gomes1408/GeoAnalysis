bee4276e8aea4635f1e21280df7eb46f64cb3dc8
Gomes1408 <68618279+Gomes1408@users.noreply.github.com>
2022-11-03 06:57:44 -0300

    Sistema de salvar completo

--- Main.py
+++ Main.py
@@ -1,10 +1,18 @@
-from tkinter import *
-from tkinter import filedialog as fd
-import tkinter.ttk as ttk
-import pandas as pd
-import csv
 
-window=Tk() 
+import tkinter as tk
+from tkinter import filedialog as fd
+from tkinter import messagebox, ttk
+import csv
+import pandas as pd
+
+class DataClass:
+    def __init__(self, head, row):
+        self.head = head
+        self.row = row
+
+window=tk.Tk() 
+
+meta = DataClass
 
 width = 1000
 height = 600
@@ -14,92 +22,59 @@ x = (screen_width/2) - (width/2)
 y = (screen_height/2) - (height/2)
 window.geometry("%dx%d+%d+%d" % (width, height, x, y))
 window.resizable(0, 0)
+frame1 = tk.LabelFrame(window, text="Excel Data")
+frame1.place(height=400, width=1000)
+
 
 
 def askOpenFile():
         data = fd.askopenfilename(title='Abrir arquivo de projeto', initialdir='/', filetypes = (("Csv Files", '*.csv'),('All files', '*.*')))
-        with open(data) as f:
-            reader = csv.DictReader(f, delimiter=';')
-            for row in reader:
-                Comment = row['Comment']
-                K2O = row['K2O']
-                TiO2 = row['TiO2']
-                FeO = row['FeO']
-                MnO = row['MnO']
-                CaO = row['CaO']
-                Cr2O3 = row['Cr2O3']
-                Al2O3 = row['Al2O3']
-                SiO2 = row['SiO2']
-                MgO = row['MgO']
-                Total = row['Total']
-                Date = row['Date']
-                tree.insert("", 0, values=(Comment, K2O, TiO2, FeO, MnO, CaO, Cr2O3, Al2O3, SiO2, MgO, Total, Date))
+        df = pd.read_csv(data, delimiter=';')
+        meta.head = list(df.columns)
+        tree["column"] = meta.head
+        tree["show"] = "headings"
+        for column in tree["columns"]:
+            tree.heading(column, text=column) 
+
+        meta.row = df.to_numpy().tolist() 
+        for row in meta.row:
+            tree.insert("", "end", values=row)
+        return None
 
 def saveOpenFile():
     filePath = fd.asksaveasfilename(title='Salvar arquivo de projeto', initialdir='/', filetypes = (("Csv Files", '*.csv'),('All files', '*.*')))
+    if filePath[-4] != ".csv":
+        filePath = filePath + ".csv"
+    
+    with open(filePath, 'w', encoding="utf-8", newline='') as newFile:
+        writer = csv.writer(newFile, delimiter=';')
+        writer.writerow(meta.head)
+        for rows in meta.row:
+            writer.writerow(rows)
 
-    filePath = filePath + ".csv"
-
-    newFile = open(filePath, 'w', encoding="utf-8", newline='')
-
-    fieldnames= ['Comment', 'K2O', 'TiO2', 'FeO', 'MnO', 'CaO', 'Cr2O3', 'Al2O3', 'SiO2', 'MgO', 'Total', 'Date']
-    writer = csv.DictWriter(newFile, fieldnames=fieldnames, delimiter=';')
-    writer.writeheader()
-    #writer.writerows(data)
     newFile.close()
+    return None
 
 
-menubar = Menu(window)
-filemenu = Menu(menubar, tearoff=0)
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Open", command=askOpenFile)
-filemenu.add_command(label="Save", command=saveOpenFile)
+menubar = tk.Menu(window)
+filemenu = tk.Menu(menubar, tearoff=0)
+filemenu = tk.Menu(menubar, tearoff=0)
+filemenu.add_command(label="Open", command= lambda: askOpenFile())
+filemenu.add_command(label="Save", command= lambda: saveOpenFile())
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=window.quit)
 menubar.add_cascade(label="File", menu=filemenu)
-editmenu = Menu(menubar, tearoff=0)
+editmenu = tk.Menu(menubar, tearoff=0)
 window.config(menu=menubar)
 
-TableMargin = Frame(window, width=500)
-TableMargin.pack(side=TOP)
+tree = ttk.Treeview(frame1)
+tree.place(relheight=1, relwidth=1)
 
-scrollbarx = Scrollbar(TableMargin, orient=HORIZONTAL)
-scrollbary = Scrollbar(TableMargin, orient=VERTICAL)
-tree = ttk.Treeview(TableMargin, columns=("Comment", "K2O", "TiO2", "FeO", "MnO", "CaO", "Cr2O3", "Al2O3", "SiO2", "MgO", "Total", "Date"), 
-height=600, selectmode="extended", yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
-
-scrollbary.config(command=tree.yview)
-scrollbary.pack(side=RIGHT, fill=Y)
-scrollbarx.config(command=tree.xview)
-scrollbarx.pack(side=BOTTOM, fill=X)
-
-tree.heading('Comment', text="Comment", anchor=W)
-tree.heading('K2O', text="K2O", anchor=W)
-tree.heading('TiO2', text="TiO2", anchor=W)
-tree.heading('FeO', text="FeO", anchor=W)
-tree.heading('MnO', text="MnO", anchor=W)
-tree.heading('CaO', text="CaO", anchor=W)
-tree.heading('Cr2O3', text="Cr2O3", anchor=W)
-tree.heading('Al2O3', text="Al2O3", anchor=W)
-tree.heading('SiO2', text="SiO2", anchor=W)
-tree.heading('MgO', text="MgO", anchor=W)
-tree.heading('Total', text="Total", anchor=W)
-tree.heading('Date', text="Date", anchor=W)
-tree.column('#0', stretch=NO, minwidth=0, width=0)
-tree.column('#1', stretch=NO, minwidth=200, width=250)
-tree.column('#2', stretch=NO, minwidth=50, width=50)
-tree.column('#3', stretch=NO, minwidth=50, width=50)
-tree.column('#4', stretch=NO, minwidth=50, width=50)
-tree.column('#5', stretch=NO, minwidth=50, width=50)
-tree.column('#6', stretch=NO, minwidth=50, width=50)
-tree.column('#7', stretch=NO, minwidth=50, width=50)
-tree.column('#8', stretch=NO, minwidth=50, width=50)
-tree.column('#9', stretch=NO, minwidth=50, width=50)
-tree.column('#10', stretch=NO, minwidth=50, width=50)
-tree.column('#11', stretch=NO, minwidth=50, width=50)
-tree.pack()
-
-
+treescrolly = tk.Scrollbar(frame1, orient="vertical", command=tree.yview) 
+treescrollx = tk.Scrollbar(frame1, orient="horizontal", command=tree.xview) 
+tree.configure(xscrollcommand=treescrollx.set, yscrollcommand=treescrolly.set) 
+treescrollx.pack(side="bottom", fill="x") 
+treescrolly.pack(side="right", fill="y")
 
 if __name__ == "__main__":
     window.mainloop()
